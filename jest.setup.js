80302f8a67531eab2ee49b0f98a4d697a058cd77
unknown <jaiswalsanket244@gmail.com>
2023-11-04 20:33:30 +0530

    fixed

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-// jest.setup.js
-const { MongoMemoryServer } = require('mongodb-memory-server');
-const mongod = new MongoMemoryServer();
-
-module.exports = async () => {
-  const uri = await mongod.getUri();
-  process.env.MONGOURL = uri;
-};
